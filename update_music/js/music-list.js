80a4e349cdfcbe736017c3192343cc91a2ebd36f
HUSEN ALGIFARI <93850135+Husen844algifari@users.noreply.github.com>
2022-09-21 15:41:25 +0800

    Add files via upload

---
+++ update_music/js/music-list.js
@@ -0,0 +1,63 @@
+let allMusic = [
+  {
+    name: "Midua Cinta",
+    artist: "Audio Library",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Forever Young",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Casablanca",
+    artist: "Audio Library",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Marvin Gaye",
+    artist: "Audio Library",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Peace Be Upon You",
+    artist: "Audio Library",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Until I Found You",
+    artist: "Audio Library",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Dessert",
+    artist: "Audio Library",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Pilihan Hatiku",
+    artist: "Audio Library",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Merindu Lagi",
+    artist: "Audio Library",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+  name: "Life Of The Party",
+  artist: "Audio Library",
+  img: "music-10",
+  src: "music-10"
+},
+  
+];
